14e6c0753c5148de55d081ee420406fd800a6341
James <james@jarofgreen.co.uk>
2011-12-28 10:54:18 +0000

    About text was not scrollable, and links were not clickable. Both fixed.

--- gen/uk/co/jarofgreen/JustADamnCompass/R.java
+++ gen/uk/co/jarofgreen/JustADamnCompass/R.java
@@ -15,7 +15,8 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int about=0x7f060000;
+        public static final int about=0x7f060001;
+        public static final int about_text=0x7f060000;
     }
     public static final class layout {
         public static final int about=0x7f030000;
--- src/uk/co/jarofgreen/JustADamnCompass/AboutActivity.java
+++ src/uk/co/jarofgreen/JustADamnCompass/AboutActivity.java
@@ -2,6 +2,9 @@ package uk.co.jarofgreen.JustADamnCompass;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity {
 
@@ -9,6 +12,10 @@ public class AboutActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
+		
+		TextView tv = (TextView) findViewById(R.id.about_text);
+		tv.setMovementMethod(new ScrollingMovementMethod());
+		Linkify.addLinks(tv, Linkify.WEB_URLS);		
 	}
 	
 }
