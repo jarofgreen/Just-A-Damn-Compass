384c9b3e0273cb94be0d94e0637f6eb74515a131
Live session user <ubuntu@ubuntu.(none)>
2013-06-06 11:03:44 -0400

    removed jitter, new compass image, invertability, title bar

--- gen/uk/co/jarofgreen/JustADamnCompass/R.java
+++ gen/uk/co/jarofgreen/JustADamnCompass/R.java
@@ -11,12 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int compass=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int background=0x7f020000;
+        public static final int background_black=0x7f020001;
+        public static final int compass=0x7f020002;
+        public static final int compass_black=0x7f020003;
+        public static final int compass_white=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
     }
     public static final class id {
-        public static final int about=0x7f060001;
+        public static final int about=0x7f060002;
         public static final int about_text=0x7f060000;
+        public static final int invert=0x7f060001;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -25,8 +30,9 @@ public final class R {
         public static final int main=0x7f050000;
     }
     public static final class string {
-        public static final int about_menu=0x7f040001;
-        public static final int about_text=0x7f040002;
+        public static final int about_menu=0x7f040002;
+        public static final int about_text=0x7f040003;
         public static final int app_name=0x7f040000;
+        public static final int invert_menu=0x7f040001;
     }
 }
